9004cf722c0280e3f649b52a923f7f37c4d42bd1
Smart Robos <robosinctech@gmail.com>
2017-10-25 19:55:16 +0530

    Add files via upload

---
+++ cards/cards.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterialModule} from './materialmodule';
+
+@Component({
+    selector: 'cards',
+    templateUrl: 'cards.component.html',
+    styleUrls: ['cards.component.scss'],
+   
+})
+export class CardsComponent implements OnInit {
+    constructor() { }
+    ngOnInit() {}
+}
---
+++ cards/main.ts
@@ -0,0 +1,29 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule}  from './materialmodule'
+import {CardsComponent} from './cards.component
+import {HttpModule} from '@angular/http';
+
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    ReactiveFormsModule,
+ 
+  ],
+
+  declarations: [CardsComponent],
+  bootstrap: [CardsComponent],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
