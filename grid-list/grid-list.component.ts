cff156c7682f989bc75abd76c7a924bbe53ce26e
Smart Robos <robosinctech@gmail.com>
2017-10-26 12:06:39 +0530

    Add files via upload

---
+++ grid-list/grid-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MaterialModule} from './materialmodule';
+
+@Component({
+  selector: 'grid-list',
+  templateUrl: 'grid-list.component.html',
+  styleUrls: ['grid-list.component.scss'],
+})
+  
+export class GridListComponent  {
+
+  tiles: any[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+}
