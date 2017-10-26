fe2c243281dbd2f9b55291574fdea1eaac910745
Smart Robos <robosinctech@gmail.com>
2017-10-26 12:20:22 +0530

    Add files via upload

---
+++ sidenav/main.ts
@@ -0,0 +1,29 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule}  from './materialmodule'
+import {SideNavComponent} from './sidenav.component';
+import {HttpModule} from '@angular/http';
+
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    ReactiveFormsModule,
+ 
+  ],
+
+  declarations: [SideNavComponent],
+  bootstrap: [SideNavComponent],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ sidenav/sidenav.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+ 
+})
+export class SideNavComponent {
+
+  constructor() { }
+
+}
