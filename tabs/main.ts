ea481cd9244a31d6fa7caaf9cc18e212545a0b69
Smart Robos <robosinctech@gmail.com>
2017-10-26 12:26:53 +0530

    Add files via upload

---
+++ tabs/main.ts
@@ -0,0 +1,28 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule}  from './materialmodule'
+import {TabsComponent} from './tabs.component';
+import {HttpModule} from '@angular/http';
+
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+
+  declarations: [TabsComponent],
+  bootstrap: [TabsComponent],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ tabs/tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterialModule} from './materialmodule';
+
+@Component({
+    selector: 'tabs',
+    templateUrl: 'tabs.component.html',
+    styleUrls: ['tabs.component.scss'],
+   
+})
+export class TabsComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
