eb95aef30ab28b7ca36b95b6913cf5f79e195b8f
Smart Robos <robosinctech@gmail.com>
2017-10-26 12:12:43 +0530

    Add files via upload

---
+++ menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {MaterialModule} from './materialmodule';
+
+
+@Component({
+  selector: 'menu',
+  templateUrl: 'menu.component.html',
+  styleUrls: ['menu.component.scss'],
+ 
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
